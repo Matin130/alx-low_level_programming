2c9329f91983b255cbe343f7df0f1d70588e3981
Matin <olayemiolamilekan05@gmail.com>
2023-10-05 19:36:03 -0700

    oh well

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
