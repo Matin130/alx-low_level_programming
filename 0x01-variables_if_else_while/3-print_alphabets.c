ffcaf354fd14a3e5a63dc995e9019ba806b869ea
Matin <olayemiolamilekan05@gmail.com>
2023-10-08 19:42:20 -0700

    psychooo...

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the alphabet in lowercase, and then in uppercase
+ * Return - 0
+ */
+
+int main(void)
+{
+	char alpha;
+	char caps;
+	for (caps = 'a'; caps <= 'z'; caps++)	
+		putchar(caps);
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
