151bc30a7d7519c58437d8ce9f2a861fc1d804bf
Matin <olayemiolamilekan05@gmail.com>
2023-10-13 02:21:39 -0700

    finally

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - program that prints exactly "and that piece of art is useful"
@@ -8,6 +9,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
