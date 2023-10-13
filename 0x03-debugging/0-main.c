bdf26ed7a776a78ddcd38c99156a996d51ffe22f
Matin <olayemiolamilekan05@gmail.com>
2023-10-13 15:22:44 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * main - tests for negetive or positive numbers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	positive_or_negative(n);
+	return (0);
+}
