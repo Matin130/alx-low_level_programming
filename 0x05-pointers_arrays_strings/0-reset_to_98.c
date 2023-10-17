7b22b46cfadc30b5d49b0ad5308b55107d0f9846
Matin <olayemiolamilekan05@gmail.com>
2023-10-16 18:46:57 -0700

    Reset..

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * function that takes a pointer to an int as parameter
+ * reset_to_98 - function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * @*n: parameter used
+ * @n: parameter used
  * Return: 0
  */
 
