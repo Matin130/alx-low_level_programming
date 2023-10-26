e37ff2695cb4b3aef1c688bbd9d7446d6d057791
Matin <olayemiolamilekan05@gmail.com>
2023-10-25 18:36:48 -0700

    to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: string passed
+ * Return: str
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] > 96)
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
