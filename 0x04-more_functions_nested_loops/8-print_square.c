d2aabe07b3b34224b300c18ed8ff43f931c6ab99
Matin <olayemiolamilekan05@gmail.com>
2023-11-04 03:30:38 -0700

    Square drawing

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - print_square
+ * @size: the size of the square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
