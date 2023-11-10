e8676c6d0e7704ff55bc80393d7f5a3e9fff1db9
Matin <olayemiolamilekan05@gmail.com>
2023-11-10 00:44:32 -0800

    capitalization

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,11 +2,30 @@
 
 /**
  * cap_string - capitalizes all words of a string.
- * @str: string passed
- * Return: str
+ * @s: string passed
+ * Return: s
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
+	int arr_index;
 
+	for (arr_index = 0; s[arr_index] != '\0'; arr_index++)
+	{
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] = s[0] - 32;
+
+		if (s[arr_index] == ' ' || s[arr_index] == '\t'
+			|| s[arr_index] == '\n' || s[arr_index] == ','
+			|| s[arr_index] == ';' || s[arr_index] == '.'
+			|| s[arr_index] == '!' || s[arr_index] == '?'
+			|| s[arr_index] == '"' || s[arr_index] == '('
+			|| s[arr_index] == ')' || s[arr_index] == '{'
+			|| s[arr_index] == '}')
+		{
+			if (s[arr_index + 1] >= 97 && s[arr_index + 1] <= 122)
+				s[arr_index + 1] = s[arr_index + 1] - 32;
+		}
+	}
+	return (s);
 }
