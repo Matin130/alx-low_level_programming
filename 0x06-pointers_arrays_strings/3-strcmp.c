2909afbcc3817b7d2afb1bebe0cbdfab50d33347
Matin <olayemiolamilekan05@gmail.com>
2023-11-09 22:07:35 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, f;
+	int i;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (s1[i] == s2[i])
+		if (s1[i] != s2[i])
 		{
-			f = 0;
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s1[i] > s2[i])
+				return (s1[i] - s2[i]);
 		}
-		else if (s1[i] < s2[i])
-			f--;
-		else
-			f++;
 	}
-	return (f);
+	return (0);
 }
