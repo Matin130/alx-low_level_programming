e5edea8adad5aae67dbea3263c5c2acaa42afb8b
Matin <olayemiolamilekan05@gmail.com>
2023-11-17 11:46:58 -0800

    print filename with argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,14 @@
 
 /**
  * main - prints its name, followed by a new line
- * argc: size of arguments
- * argv: array of arguments
+ * @argc: size of arguments
+ * @argv: array of arguments
+ * Return: 0
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 
-	return (0);	
+	return (0);
 }
